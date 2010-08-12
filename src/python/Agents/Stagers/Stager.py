0095b45f646bcffa457ce6696feab99c89b95812
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-12 16:42:24 +0000

    Add in some documentation References #24 and #104
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/StageManager/trunk@2505 4525493e-7705-40b1-a816-d608a930855b

--- src/python/Agents/Stagers/Stager.py
+++ src/python/Agents/Stagers/Stager.py
@@ -14,14 +14,13 @@ class Stager:
         
     def __call__(self, files=[]):
         """
-        This is where the work is done. A list of files are passed into the 
-        __call__ method and code is executed here to process each one.
+        This is where the work is done. A list of files represented by a 
+        dictionary are passed into the __call__ method and code is executed here
+        to process each one.
         """
         start_time = str(datetime.datetime.now())
         staged, incomplete, failed = self.command(files)
-        #TODO: Do we want to know the request id? It's not that meaningful given
-        # the asynchronous nature of populating the file list and the calls to 
-        # the stager __call__ method 
+        stage_end_time = str(datetime.datetime.now())
         msg = "%s files are staged, %s files are staging, %s files failed to stage"
         self.logger.info(msg % (len(staged), len(incomplete), len(failed)))
         self.mark_good(staged)
@@ -29,8 +28,13 @@ class Stager:
         self.mark_failed(failed)
         #TODO: improve the stats dict, should include total size, timing etc. #24
         end_time = str(datetime.datetime.now())
-        stats = {'good': len(staged), 'failed': len(failed),
-                 'start_time': start_time, 'end_time': end_time}
+        stats = {'good': staged, 
+                 'failed': failed, 
+                 'incomplete': incomplete,
+                 'start_time': start_time,  
+                 'stage_end_time': stage_end_time,
+                 'end_time': end_time}
+
         self.record_stats(stats)
         
         self.queuedb.commit(viewlist=['stagequeue/file_state'])
@@ -38,7 +42,16 @@ class Stager:
     def command(self, files):
         """
         A null stager - files are never staged and just fail. This should be
-        over ridden by subclasses. Return staged, incomplete, failed files.
+        over ridden by subclasses. Return lists of staged, incomplete, failed 
+        files as dictionaries. A file dict looks like:
+        # {
+        #   '_id': attrs.get('name'), 
+        #   'bytes': int(attrs.get('bytes')),
+        #   'checksum': {checksum.split(':')[0]: checksum.split(':')[1]},
+        #   'state': 'new',
+        #   'retry_count': [],
+        #   'request_id': request_id
+        # }
             staged: file is on disk
             incomplete: file has been requested but is not on the disk pool
             failed: file could not be staged - may not be supported by MSS
@@ -52,6 +65,68 @@ class Stager:
         off site)
         """
         #TODO: refresh statistics views #24
+        stats_doc = {'start_time': stats['start_time'], 
+                     'end_time': stats['end_time'], 
+                     'stage_end_time': stats['stage_end_time']}
+        
+        results ={}
+        # staged, failed and incomplete are a list of dicts, one dict per file, 
+        # a file dict looks like:
+        # {
+        #   '_id': attrs.get('name'), 
+        #   'bytes': int(attrs.get('bytes')),
+        #   'checksum': {checksum.split(':')[0]: checksum.split(':')[1]},
+        #   'state': 'new',
+        #   'retry_count': [],
+        #   'request_id': request_id
+        # }
+        # Build up per request stats for staged/failed/incomplete. Views will be
+        # used to aggregate this information. 
+        # TODO: #104
+        for file in staged:  
+          if file['request_id'] in results.keys():
+            results[file['request_id']]['good'] += 1
+            results[file['request_id']]['staged_bytes'] += file['bytes']
+          else:
+            results[file['request_id']] = {}
+            results[file['request_id']]['timestamp'] = stats['stage_end_time']
+            results[file['request_id']]['good'] = 1
+            results[file['request_id']]['staged_bytes'] = file['bytes']
+            results[file['request_id']]['failed'] = 0
+            results[file['request_id']]['failed_bytes'] = 0
+            results[file['request_id']]['incomplete'] = 0
+            results[file['request_id']]['incomplete_bytes'] = 0
+        # .. and for failed    
+        for file in failed: # [] of {}'s, same as staged
+          if file['request_id'] in results.keys():
+            results[file['request_id']]['failed'] += 1
+            results[file['request_id']]['failed_bytes'] += file['bytes']
+          else:
+            results[file['request_id']] = {}
+            results[file['request_id']]['timestamp'] = stats['stage_end_time']
+            results[file['request_id']]['failed'] = 1
+            results[file['request_id']]['failed_bytes'] = file['bytes']
+            results[file['request_id']]['good'] = 0
+            results[file['request_id']]['staged_bytes'] = 0
+            results[file['request_id']]['incomplete'] = 0
+            results[file['request_id']]['incomplete_bytes'] = 0
+        # and for incomplete    
+        for file in incomplete: # [] of {}'s, same as staged
+          if file['request_id'] in results.keys():
+            results[file['request_id']]['incomplete'] += 1
+            results[file['request_id']]['incomplete_bytes'] += file['bytes']
+          else:
+            results[file['request_id']] = {}
+            results[file['request_id']]['timestamp'] = stats['stage_end_time']
+            results[file['request_id']]['incomplete'] = 1
+            results[file['request_id']]['incomplete_bytes'] = file['bytes']
+            results[file['request_id']]['good'] = 0
+            results[file['request_id']]['staged_bytes'] = 0
+            results[file['request_id']]['failed'] = 0
+            results[file['request_id']]['failed_bytes'] = 0
+
+        stats_doc['results'] = results
+
         self.statsdb.commit(stats)
     
     def mark_good(self, files=[]):
